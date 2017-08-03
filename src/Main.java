8638a9bce48ec02c62a4b2ca903c4d051b390f7c
SuzukiRyusei <ryuusei@suzukitakashisei-no-iMac.local>
2017-08-03 20:46:22 +0900

    no message

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
+import java.util.*;
 
 /**
  * Created by ryu on 2017/07/26.
@@ -32,7 +33,8 @@ public class Main extends JFrame {
         getContentPane().add(panel,BorderLayout.EAST);
         contentPane.add(canvas, BorderLayout.WEST);
 
-//        TextArea textArea = new TextArea();
+        TextArea textArea = new TextArea();
+        textArea.setMaximumSize(new Dimension(400,300));
 
         JButton addButton = new JButton("Change points");
         addButton.addActionListener(new ActionListener() {
@@ -41,18 +43,42 @@ public class Main extends JFrame {
                 makePoints();
                 canvas.setPoints(points);
                 canvas.repaint();
-  //              textArea.setText("Change points");
+
+                textArea.append(getTimeString() + " @ Change points. Point count : "+pointNum+"\n");
             }
         });
+/*
+        JButton eraseButton = new JButton("Erase");
+        eraseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                textArea.append(getTimeString() + " @ Erase\n");
+            }
+        });
+*/
         panel.add(addButton);
-  //      panel.add(textArea,BorderLayout.SOUTH);
+//        panel.add(eraseButton);
+        panel.add(textArea,BorderLayout.SOUTH);
     }
 
     public void makePoints() {
         Random rnd = new Random();
         for (int i = 0; i < pointNum; i++) {
-            points[i] = new PointClass(rnd.nextInt(this.maxPos)+50, rnd.nextInt(this.maxPos)+300);
+            points[i] = new PointClass(rnd.nextInt(this.maxPos)+40, rnd.nextInt(this.maxPos)+300);
         }
+
+    }
+
+    public String getTimeString(){
+        int time[] = new int[3];
+        Calendar calendar = Calendar.getInstance();
+        time[0] = calendar.get(calendar.HOUR_OF_DAY);
+        time[1] = calendar.get(calendar.MINUTE);
+        time[2] = calendar.get(calendar.SECOND);
+
+        String returnS = String.format("%02d",time[0]) + ":" + String.format("%02d", time[1]) + ":" +  String.format("%02d", time[2]);
+
+        return  returnS;
     }
 
 }
